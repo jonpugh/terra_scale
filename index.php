6ae6b651bd77c73904f0639f7e8b502160f25b97
Jon Pugh <jon@thinkdrop.net>
2015-07-18 11:08:11 -0400

    adding index.

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<h3>Container IP:</h3>
+
+<?php print $_SERVER['SERVER_ADDR']; ?>
+
+<script>
+(function() {
+
+
+  setTimeout(
+    function () {
+      window.reload()
+    }, 1000
+  );
+
+
+})();
+</script>
